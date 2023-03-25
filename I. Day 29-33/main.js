5e1ef96ff4d134acb093b088255cd72ecd374663
jubayerasif <jubaerasif101@gmail.com>
2023-03-25 16:21:44 +0600

    function

--- I. Day 29-33/main.js
+++ I. Day 29-33/main.js
@@ -16,17 +16,25 @@
 // alert(hobbies[0]);
 // alert(job.title);
 
-let userName= "Arnold";
+let userName = "Arnold";
 let userAge = 75;
-let hobbies = ['Cricket', 'Cooking', 'Watching'];
+let hobbies = ["Cricket", "Cooking", "Watching"];
 let job = {
-    title: 'Cricketer',
-    place: 'America',
-    salary: 5000,
+  title: "Cricketer",
+  place: "America",
+  salary: 5000,
 };
-let adultYears = userAge - 18;
+
+let adultYears;
+
+function calculateAdultYears() {
+    adultYears = userAge - 18;
+}
+
+calculateAdultYears();
 alert(adultYears);
 
 userAge = 70;
 adultYears = userAge - 18;
+
 alert(adultYears);
