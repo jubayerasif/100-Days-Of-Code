512bba2287c830f7a22b78b4bf7903b3a89bf3bc
jubayerasif <jubaerasif101@gmail.com>
2023-03-25 13:12:20 +0600

    Upgrade

--- I. Day 29-33/main.js
+++ I. Day 29-33/main.js
@@ -1,6 +1,11 @@
-let greetingText = "Hi, I'm a JavaScript!";
-let date = "2025-01-01";
-alert(greetingText);
-greetingText = "Hi, I'm a React JavaScript!";
-alert(greetingText);
-alert(date);
+// let greetingText = "Hi, I'm a JavaScript!";
+// let date = "2025-01-01";
+// alert(greetingText);
+// greetingText = "Hi, I'm a React JavaScript!";
+// alert(greetingText);
+// alert(date);
+
+let userName= "Jubayer";
+let userAge = 18;
+let hobbies = ['Coding', 'Cooking', 'Watching'];
+alert(hobbies[0])
