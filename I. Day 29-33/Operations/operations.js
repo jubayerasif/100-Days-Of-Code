382d0fcca063c735fc02ee24004afd4467cfb775
jubayerasif <jubaerasif101@gmail.com>
2023-03-26 13:11:59 +0600

    afaf

--- I. Day 29-33/Operations/operations.js
+++ I. Day 29-33/Operations/operations.js
@@ -8,7 +8,7 @@ console.log(10 % 4);
 console.log(10 % 3);
 
 console.log((10 + 3 - 5) * 10);
-
+/*
 let result = (10 + 3 - 5) * 10;
 result = 10 * 4;
 result++; // result = result +1
@@ -20,3 +20,5 @@ result /= 5; // result = result / 5
 result *= 5; // result = result * 5
 
 console.log(result);
+*/
+console.log('Arnold' + ' ' +'Wick');
