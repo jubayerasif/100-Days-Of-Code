b8e16c0db8b9955053b3f162ee061566912aeaed
jubayerasif <jubaerasif101@gmail.com>
2023-03-26 13:10:33 +0600

    Updated

---
+++ I. Day 29-33/Operations/operations.js
@@ -0,0 +1,22 @@
+// Math Operations
+
+console.log(10 + 4);
+console.log(10 - 4);
+console.log(10 * 4);
+console.log(10 / 4);
+console.log(10 % 4);
+console.log(10 % 3);
+
+console.log((10 + 3 - 5) * 10);
+
+let result = (10 + 3 - 5) * 10;
+result = 10 * 4;
+result++; // result = result +1
+result--; // result = result -1
+
+result += 5; // result = result + 5
+result -= 5; // result = result - 5
+result /= 5; // result = result / 5
+result *= 5; // result = result * 5
+
+console.log(result);
