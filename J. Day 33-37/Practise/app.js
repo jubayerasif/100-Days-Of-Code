244d309d51adec2434ed945d446b7295bb8bec4d
jubayerasif <jubaerasif101@gmail.com>
2023-03-30 00:35:17 +0600

    ads

--- J. Day 33-37/Practise/app.js
+++ J. Day 33-37/Practise/app.js
@@ -5,7 +5,7 @@ document.body.children[1].children[0].href = 'https://google.com';
 // alert();
 // window.alert();
 
-let anchorElement =document.getElementById('external-link')';
+let anchorElement =document.getElementById('external-link');
 anchorElement.href = 'https://google.com';
 
 document.querySelector('#')
