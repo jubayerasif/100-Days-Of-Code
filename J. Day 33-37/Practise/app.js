9c6d9544a6dfac3906644e3c2b60401b65e7d977
jubayerasif <jubaerasif101@gmail.com>
2023-03-30 00:49:33 +0600

    Updated

--- J. Day 33-37/Practise/app.js
+++ J. Day 33-37/Practise/app.js
@@ -1,5 +1,5 @@
 // console.dir(window.document);
-document.body.children[1].children[0].href = 'https://google.com';
+// document.body.children[1].children[0].href = 'https://google.com';
 
 // console.dir(document);
 // alert();
@@ -8,12 +8,15 @@ document.body.children[1].children[0].href = 'https://google.com';
 let anchorElement =document.getElementById('external-link');
 anchorElement.href = 'https://google.com';
 
-document.querySelector('#')
+// document.querySelector('#')
 
 // ADD AN ELEMENT
 // 1. Create the new clement
 
 let newAnchorElement = document.createElement('a');
+newAnchorElement.href = 'https://google.com';
+newAnchorElement.target = '_blank'
+newAnchorElement.textContent = 'This is the new anchor tag';
 
 // 2. Get access to the parent element that should hold the new element
 
