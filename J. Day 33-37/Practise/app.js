0248804396b45525e3b89f3627da5ecf0ddbc82f
jubayerasif <jubaerasif101@gmail.com>
2023-03-30 00:37:18 +0600

    Up

--- J. Day 33-37/Practise/app.js
+++ J. Day 33-37/Practise/app.js
@@ -13,3 +13,4 @@ document.querySelector('#')
 // ADD AN ELEMENT
 // 1. Create the new clement
 // 2. Get access to the parent element that should hold the new element
+// 3. Insert the new element into the parent element content
