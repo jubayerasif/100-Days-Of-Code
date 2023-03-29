7a6f9b876a18a88df7414b1d782284ed4e1bb9a6
jubayerasif <jubaerasif101@gmail.com>
2023-03-30 00:36:45 +0600

    dada

--- J. Day 33-37/Practise/app.js
+++ J. Day 33-37/Practise/app.js
@@ -10,3 +10,6 @@ anchorElement.href = 'https://google.com';
 
 document.querySelector('#')
 
+// ADD AN ELEMENT
+// 1. Create the new clement
+// 2. Get access to the parent element that should hold the new element
