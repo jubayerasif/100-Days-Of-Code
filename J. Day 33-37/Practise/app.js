3bbf34e23afcd3a74b53cab79a977d314531cf46
jubayerasif <jubaerasif101@gmail.com>
2023-03-30 00:39:26 +0600

    ada

--- J. Day 33-37/Practise/app.js
+++ J. Day 33-37/Practise/app.js
@@ -13,7 +13,7 @@ document.querySelector('#')
 // ADD AN ELEMENT
 // 1. Create the new clement
 
-
+let newAnchorElement = document.createElement('a');
 
 // 2. Get access to the parent element that should hold the new element
 // 3. Insert the new element into the parent element content
