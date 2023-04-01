3ab3fbab8c414d300cc88fd05b5865ee93a90ba7
jubayerasif <jubaerasif101@gmail.com>
2023-04-01 16:37:52 +0600

    dsaf

--- J. Day 33-37/Practise_2/app.js
+++ J. Day 33-37/Practise_2/app.js
@@ -10,9 +10,11 @@ paragraphElement.addEventListener("click", changeParagraphText);
 let inputElement = document.querySelector("input");
 
 function retrieveUserInput(event) {
-  let enteredText = inputElement.value;
+  //   let enteredText = inputElement.value;
+  //   let enteredText = event.target.value;
+  let enteredText = event.data;
   console.log(enteredText);
-  console.log(event);
+  //   console.log(event);
 }
 
 inputElement.addEventListener("input", retrieveUserInput);
