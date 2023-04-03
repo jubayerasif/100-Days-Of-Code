de5030bebebd20546274c2181a00c855ed6e771f
jubayerasif <jubaerasif101@gmail.com>
2023-04-03 19:43:59 +0600

    Updated

--- J. Day 33-37/Practise_2/app.js
+++ J. Day 33-37/Practise_2/app.js
@@ -23,17 +23,17 @@ function retrieveUserInput(event) {
 inputElement.addEventListener("input", retrieveUserInput);
 */
 
-let productNameInputElement = document.getElementById('product-name');
-let remainingCharsElement = document.getElementById('remaining-chars');
+const productNameInputElement = document.getElementById('product-name');
+const remainingCharsElement = document.getElementById('remaining-chars');
 
-let maxAllowedChars = productNameInputElement.maxLength;
+const maxAllowedChars = productNameInputElement.maxLength;
 
 function updateRemainingCharacters(){
-    let enteredText = event.target.value;
-    let enteredTextLength = enteredText.length;
+    const enteredText = event.target.value;
+    const enteredTextLength = enteredText.length;
 
 
-    let remainingCharacters = maxAllowedChars - enteredTextLength;
+    const remainingCharacters = maxAllowedChars - enteredTextLength;
 
     remainingCharsElement.textContent = remainingCharacters;
 }
