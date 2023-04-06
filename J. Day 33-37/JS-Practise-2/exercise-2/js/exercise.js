bd437e9f2acfc104aec0b93c24f8faa059bb1cd9
jubayerasif <jubaerasif101@gmail.com>
2023-04-06 08:35:33 +0600

    Updated

--- J. Day 33-37/JS-Practise-2/exercise-2/js/exercise.js
+++ J. Day 33-37/JS-Practise-2/exercise-2/js/exercise.js
@@ -11,16 +11,16 @@ const secondButtonElement = document.getElementById('change-bg-btn');
 //    - Output the first button by using the variable in which it's stored
 //    - Output the second button WITHOUT using the variable in which it's stored
 
-function removeParagraph() {
-    console.dir(firstButtonElement);
-}
+// function removeParagraph() {
+//     console.dir(firstButtonElement);
+// }
 
-function changeBackgroundColor(event) {
-    console.dir(event.target);
-}
+// function changeBackgroundColor(event) {
+//     console.dir(event.target);
+// }
 
-firstButtonElement.addEventListener('click', removeParagraph);
-secondButtonElement.addEventListener('click', changeBackgroundColor);
+// firstButtonElement.addEventListener('click', removeParagraph);
+// secondButtonElement.addEventListener('click', changeBackgroundColor);
 
 // 3) Now select and store the paragraphs mentioned in the text you see on the page
 //    (first and third paragraph)
@@ -28,11 +28,27 @@ secondButtonElement.addEventListener('click', changeBackgroundColor);
 //      mentioned elements
 //    - If you struggle with DOM drilling, use "ids" instead but watch the solution!
 
+const firstParagraphElement = document.body.children[2].children[1];
+console.log(firstParagraphElement);
+// const thirdParagraphElement = document.body.children[2].children[3];
+const thirdParagraphElement = firstParagraphElement.nextElementSibling.nextElementSibling;
+console.log(thirdParagraphElement);
 
 // 4) Change the functions from (2) such that:
 //    - The first button removes the third paragraph (i.e. the <p> prior to it)
 //    - The second button changes the background color of the first paragraph to blue
 
+function removeParagraph() {
+    thirdParagraphElement.remove();
+}
+
+function changeBackgroundColor(event) {
+    // firstParagraphElement.style.backgroundColor = 'blue';
+    
+}
+
+firstButtonElement.addEventListener('click', removeParagraph);
+secondButtonElement.addEventListener('click', changeBackgroundColor);
 
 // 5) Solve (4) both by changing the "inline styles" as well as by adding CSS classes
 
