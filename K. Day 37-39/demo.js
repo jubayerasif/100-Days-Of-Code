1cc6c65fd25694d9ee88e09944590961227fe64d
jubayerasif <jubaerasif101@gmail.com>
2023-04-08 16:03:21 +0600

    Updated

--- K. Day 37-39/demo.js
+++ K. Day 37-39/demo.js
@@ -16,6 +16,9 @@ function updateRemainingCharacters(event) {
   if (remainingCharacters <= 10) {
     remainingCharsElement.classList.add('warning');
     productNameInputElement.classList.add('warning');
+  } else {
+    remainingCharsElement.classList.remove('warning');
+    productNameInputElement.classList.remove('warning');
   }
 }
 
