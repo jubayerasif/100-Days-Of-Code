c4fec27653c381b8cb5d80ace1c470e589f1e45e
jubayerasif <jubaerasif101@gmail.com>
2023-04-13 23:27:43 +0600

    Updated

--- K. Day 37-39/loops.js
+++ K. Day 37-39/loops.js
@@ -1,6 +1,23 @@
 for (let i = 0; i < 10; i++) {
-    console.log(i);
+  console.log(i);
 }
 
-const users = ['Any', 'Max', 'Joel'];
+const users = ["Any", "Max", "Joel"];
 
+for (const user of users) {
+  console.log(user);
+}
+// for (let i = 0; i < users.length; i++) {
+//     console.log(users[i]);
+// }
+
+const loggedInUser = {
+  name: "Jubayer",
+  age: 18,
+  isAdmin: true,
+};
+
+for(const propertyName in loggedInUser){
+    console.log(propertyName);
+    console.log(loggedInUser[propertyName]);
+}
