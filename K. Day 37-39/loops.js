38bb4cf113cd5cc07b5997e09c058b90e6361230
jubayerasif <jubaerasif101@gmail.com>
2023-04-13 23:32:25 +0600

    Updated

--- K. Day 37-39/loops.js
+++ K. Day 37-39/loops.js
@@ -21,3 +21,11 @@ for(const propertyName in loggedInUser){
     console.log(propertyName);
     console.log(loggedInUser[propertyName]);
 }
+
+let isFinished = false;
+
+while(!isFinished){
+    isFinished = confirm('Do you want to quit?');
+}
+
+console.log('Done!');
