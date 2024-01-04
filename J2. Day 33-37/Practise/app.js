38eefc1b32d44151a7274d2b90a3fb6d05180350
jubayerasif <jubaerasif101@gmail.com>
2024-01-04 17:21:43 +0600

    ////

--- J2. Day 33-37/Practise/app.js
+++ J2. Day 33-37/Practise/app.js
@@ -1,12 +1,12 @@
 // console.dir(window.document);
-document.body.children[1].children[0].href = 'https://google.com';
+// document.body.children[1].children[0].href = 'https://google.com';
 
 // // console.dir(document);
 // // alert();
 // // window.alert();
 
-// let anchorElement = document.getElementById("external-link");
-// anchorElement.href = "https://google.com";
+let anchorElement = document.getElementById("external-link");
+anchorElement.href = "https://google.com";
 
 // // document.querySelector('#')
 
