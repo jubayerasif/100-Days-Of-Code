8870ece58196cc5dd1f610800812425789c94aaf
jubayerasif <jubaerasif101@gmail.com>
2024-01-04 15:22:30 +0600

    Folder push

---
+++ K2. Day 37-39/conditional-code.js
@@ -0,0 +1,17 @@
+const myName = "Jubayer";
+
+if (myName === 'Jubayer') {
+  console.log("Hello!", myName);
+}
+
+let isLoggedIn = true;
+
+if(isLoggedIn === true){
+  console.log('User is NOT logged in!');
+}
+
+const enteredUserName = '';
+
+if(enteredUserName){
+  console.log('Input is valid');
+}
