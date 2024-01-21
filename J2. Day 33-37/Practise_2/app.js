94ec4705707fb194a8d1df2021b8c70ed207429b
jubayerasif <jubaerasif101@gmail.com>
2024-01-22 01:23:11 +0600

    .....

--- J2. Day 33-37/Practise_2/app.js
+++ J2. Day 33-37/Practise_2/app.js
@@ -21,22 +21,21 @@
 
 // inputElement.addEventListener("input", retrieveUserInput);
 
-
-const productNameInputElement = document.getElementById('product-name');
-const remainingCharsElement = document.getElementById('remaining-chars');
+const productNameInputElement = document.getElementById("product-name");
+const remainingCharsElement = document.getElementById("remaining-chars");
 
 const maxAllowedChars = productNameInputElement.maxLength;
 
 function updateRemainingCharacters() {
-    const enteredText = event.target.value;
-    const enteredTextLength = enteredText.length;
+  const enteredText = event.target.value;
+  const enteredTextLength = enteredText.length;
 
-    const remainingCharacters = maxAllowedChars - enteredTextLength;
+  const remainingCharacters = maxAllowedChars - enteredTextLength;
 
-    remainingCharsElement.textContent = remainingCharacters;
+  remainingCharsElement.textContent = remainingCharacters;
 }
 
-productNameInputElement.addEventListener('input', updateRemainingCharacters)
+productNameInputElement.addEventListener("input", updateRemainingCharacters);
 
-const spanElement = document.getElementById('remaining-chars');
-spanElement.style.color = 'rgb(0, 120, 50)';
+const spanElement = document.getElementById("remaining-chars");
+spanElement.style.color = "rgb(0, 120, 50)";
