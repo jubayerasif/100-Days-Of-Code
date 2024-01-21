fc505e16434baa62ed76c3b9433d43798ff5e4d9
jubayerasif <jubaerasif101@gmail.com>
2024-01-21 13:34:07 +0600

    learning

--- J2. Day 33-37/Practise/app.js
+++ J2. Day 33-37/Practise/app.js
@@ -8,39 +8,39 @@
 let anchorElement = document.getElementById("external-link");
 anchorElement.href = "https://google.com";
 
-document.querySelector('#')
+// document.querySelector('#')
 
 // // ADD AN ELEMENT
 // // 1. Create the new clement
 
-// let newAnchorElement = document.createElement("a");
-// newAnchorElement.href = "https://google.com";
-// newAnchorElement.target = "_blank";
-// newAnchorElement.textContent = "This is the new anchor tag";
+let newAnchorElement = document.createElement("a");
+newAnchorElement.href = "https://google.com";
+newAnchorElement.target = "_blank";
+newAnchorElement.textContent = "This is the new anchor tag";
 
 // // 2. Get access to the parent element that should hold the new element
 
-// let firstParagraph = document.querySelector("p");
+let firstParagraph = document.querySelector("p");
 
 // // 3. Insert the new element into the parent element content
 
-// firstParagraph.append(newAnchorElement);
+firstParagraph.append(newAnchorElement);
 
 // // REMOVE ELEMENTS
 // // 1. Select the element that should be removed
 
-// let firstH1Element = document.querySelector("h1");
+let firstH1Element = document.querySelector("h1");
 
 // // 2. Remove it
 
 // firstH1Element.remove();
-// // firstH1Element.parentElement.removeChild(firstH1Element); // for older browser
+firstH1Element.parentElement.removeChild(firstH1Element); // for older browser
 
 // // MOVE ELEMENTS
 
-// firstParagraph.parentElement.append(firstParagraph);
+firstParagraph.parentElement.append(firstParagraph);
 
 // // innerHTML
 
-// firstParagraph.innerHTML = "<p>Hi there! <strong>JS is important</strong></p>";
+firstParagraph.innerHTML = "<p>Hi there! <strong>JS is important</strong></p>";
 
