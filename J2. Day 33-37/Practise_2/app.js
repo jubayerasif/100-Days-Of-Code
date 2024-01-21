f453b2dee8ce11193d8d6afb28256c19d2009ab6
jubayerasif <jubaerasif101@gmail.com>
2024-01-22 01:17:02 +0600

    ....

--- J2. Day 33-37/Practise_2/app.js
+++ J2. Day 33-37/Practise_2/app.js
@@ -30,7 +30,7 @@ const remainingCharsElement = document.getElementById('remaining-chars');
 
 const maxAllowedChars = productNameInputElement.maxLength;
 
-function updateRemainingCharacters(){
+function updateRemainingCharacters() {
     const enteredText = event.target.value;
     const enteredTextLength = enteredText.length;
 
