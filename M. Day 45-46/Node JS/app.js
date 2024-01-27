3dc136208d11fc441d4222c1e88672caa7da7a31
jubayerasif <jubaerasif101@gmail.com>
2024-01-27 13:31:50 +0600

    Updated

---
+++ M. Day 45-46/Node JS/app.js
@@ -0,0 +1,14 @@
+const http = require("http");
+
+function handleRequest(request, response) {
+  response.statusCode = 200;
+  response.end("<h1>Hello World!</h1>");
+}
+
+const server = http.createServer(handleRequest);
+
+server.listen(3000);
+
+
+/// ctrl alt = n to run the code
+/// clt + c to stop the code
