030d2a9c9ec8065a67f48fe6cecf0d31aa0bbc1b
jubayerasif <jubaerasif101@gmail.com>
2024-01-27 19:48:44 +0600

    Updated

--- M. Day 45-46/Node JS/app.js
+++ M. Day 45-46/Node JS/app.js
@@ -1,17 +1,39 @@
-const http = require("http");
+// const http = require("http");
 
-function handleRequest(request, response) {
-  response.statusCode = 200;
-  response.end("<h1>Hello World!</h1>");
-}
+// function handleRequest(request, response) {
+//   if (request.url === "/current-time") {
+//     response.statusCode = 200;
+//     response.end("<h1>" + new Date().toISOString() + "</h1>");
+//   } else if (request.url === "/") {
+//     response.statusCode = 200;
+//     response.end("<h1>Hello World!</h1>");
+//   }
+//   // localhost:3000/current-time
+//   // localhost:3000
+// }
 
-const server = http.createServer(handleRequest);
+// const server = http.createServer(handleRequest);
 
-server.listen(3000);
-
+// server.listen(3000);
 
 /// ctrl alt = n to run the code
 /// clt + c to stop the code
 
+function handleRequest(request, response) {
+  console.log("Received request for:", request.url);
 
+  if (request.url === "/current-time") {
+    response.setHeader("Content-Type", "text/plain");
+    response.statusCode = 200;
+    response.end(new Date().toISOString());
+  } else if (request.url === "/") {
+    response.setHeader("Content-Type", "text/html");
+    response.statusCode = 200;
+    response.end("<h1>Hello World!</h1>");
+  } else {
+    response.setHeader("Content-Type", "text/plain");
+    response.statusCode = 404;
+    response.end("Not Found");
+  }
+}
 
