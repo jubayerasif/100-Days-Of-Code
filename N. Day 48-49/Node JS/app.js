09059453ce51ea0b20756656aaa85ce1083ed525
jubayerasif <jubaerasif101@gmail.com>
2024-01-29 12:04:53 +0600

    .........

--- N. Day 48-49/Node JS/app.js
+++ N. Day 48-49/Node JS/app.js
@@ -2,16 +2,25 @@ const express = require("express");
 
 const app = express();
 
-app.get("/currenttime", function(req, res){
+app.use(express.urlencoded());
+
+app.get("/currenttime", function (req, res) {
   res.send("<h1>" + new Date().toISOString() + "</h1>");
 }); // localhost:3000 /currenttime
 
-app.get("/", function(req, res){
-  res.end("<form action='store-user' method='POST;><label>Your Name:<input type= 'text'></label><button>Submit</button></form>");
+app.get("/", function (req, res) {
+  res.send(
+    "<form action='/store-user' method='POST'><label>Your Name:</label><input type= 'text'><button>Submit</button></form>"
+  );
 }); // localhost:3000
 
-app.listen(3000); // Start the server on port 3000
+app.post("/store-user", function (req, res) {
+  const userName = req.body.username;
+  console.log(userName);
+  res.send("<h1>Username Stored!</h1>");
+});
 
+app.listen(3000); // Start the server on port 3000
 
 // function handleRequest(request, response) {
 //   if (request.url === "/currenttime") {
