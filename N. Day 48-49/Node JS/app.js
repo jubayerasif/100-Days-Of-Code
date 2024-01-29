267bf86bc9b7c1556f9fc7bbbaf4d1bf422101f9
jubayerasif <jubaerasif101@gmail.com>
2024-01-29 11:44:02 +0600

    ......

--- N. Day 48-49/Node JS/app.js
+++ N. Day 48-49/Node JS/app.js
@@ -3,21 +3,25 @@ const express = require("express");
 const app = express();
 
 app.get("/currenttime", function(req, res){ 
-  
-});// localhost:3000 /currenttime
+  res.send("<h1>" + new Date().toISOString() + "</h1>");
+}); // localhost:3000 /currenttime
+
+app.get("/", function(req, res){ 
+  response.end("<h1>Hello World!</h1>");
+}); // localhost:3000
 
 app.listen(3000);
 
 
-function handleRequest(request, response) {
-  if (request.url === "/currenttime") {
-    response.statusCode = 200;
-    response.end("<h1>" + new Date().toISOString() + "</h1>");
-  } else if (request.url === "/") {
-    response.statusCode = 200;
-    response.end("<h1>Hello World!</h1>");
-  }
-}
+// function handleRequest(request, response) {
+//   if (request.url === "/currenttime") {
+//     response.statusCode = 200;
+//     response.end("<h1>" + new Date().toISOString() + "</h1>");
+//   } else if (request.url === "/") {
+//     response.statusCode = 200;
+//     response.end("<h1>Hello World!</h1>");
+//   }
+// }
 
 const server = http.createServer(handleRequest);
 
