7620e73478bbc87c1f0639ba1ca78ee7811e2e60
jubayerasif <jubaerasif101@gmail.com>
2024-01-30 12:52:05 +0600

    Updated

---
+++ N. Day 48-49/Day 49 project/app.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+app.get('/', function(req, res){
+    res.send('<h1>Hello world</h1>');
+});
+app.get('/restaurants', function(req, res){
+    const htmlFilePath = path.join(__dirname, 'views', 'restaurants.html')
+    res.sendFile(htmlFilePath);
+});
+app.listen(3000);
