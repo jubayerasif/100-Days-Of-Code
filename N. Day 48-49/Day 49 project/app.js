544a8465f531c225a26834a2069106ceb4e55b40
jubayerasif <jubaerasif101@gmail.com>
2024-01-30 13:02:32 +0600

    Updated

--- N. Day 48-49/Day 49 project/app.js
+++ N. Day 48-49/Day 49 project/app.js
@@ -4,10 +4,24 @@ const express = require('express');
 const app = express();
 
 app.get('/', function(req, res){
-    res.send('<h1>Hello world</h1>');
+    const htmlFilePath = path.join(__dirname, 'views', 'index.html');
+    res.sendFile(htmlFilePath);
 });
 app.get('/restaurants', function(req, res){
-    const htmlFilePath = path.join(__dirname, 'views', 'restaurants.html')
+    const htmlFilePath = path.join(__dirname, 'views', 'restaurants.html');
+    res.sendFile(htmlFilePath);
+});
+app.get('/recommend', function(req, res){
+    const htmlFilePath = path.join(__dirname, 'views', 'recommend.html');
+    res.sendFile(htmlFilePath);
+});
+app.get('/confirm', function(req, res){
+    const htmlFilePath = path.join(__dirname, 'views', 'confirm.html');
+    res.sendFile(htmlFilePath);
+});
+
+app.get('/about', function(req, res){
+    const htmlFilePath = path.join(__dirname, 'views', 'about.html');
     res.sendFile(htmlFilePath);
 });
 app.listen(3000);
