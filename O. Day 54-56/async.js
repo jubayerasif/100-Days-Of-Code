b78f3418d700a8a1c5e1b79e380c552e1e51026c
jubayerasif <jubaerasif101@gmail.com>
2024-03-04 23:54:37 +0600

    Introducing Promises,  Asynchronous Code & Error Handling

--- O. Day 54-56/async.js
+++ O. Day 54-56/async.js
@@ -1,12 +1,27 @@
-const fs = require("fs");
+const fs = require("fs/promises");
 
 function readFile() {
   let fileData;
-  
-  fs.readFile("data.txt", function(error, fileData){
-    console.log("File read successfully!");
-    console.log(fileData.toString());
-  });
+
+  //   fs.readFile("data.txt", function(error, fileData){
+  // if(error){
+  //     ...
+  // }
+  //     console.log("File read successfully!");
+  //     console.log(fileData.toString());
+  //     // start another async task that sends the data to a database
+  //   });
+
+  fs.readFile("data.txt")
+    .then(function (fileData) {
+      console.log("File read successfully!");
+      console.log(fileData.toString());
+      // return anotherAsyncOperation;
+    })
+    .then(function () {});
+    .cath(function (error) {
+        console.log(error);
+    });
 
   console.log("Hi there!");
 }
